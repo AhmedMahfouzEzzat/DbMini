f925a5bd480c8a727c1d42e5ac474794c8060c7c
ahmed <ahmed.alaa.cis@gmail.com>
2017-12-13 20:12:55 +0200

    write file

--- MiniDatabaseGui/Form1.cs
+++ MiniDatabaseGui/Form1.cs
@@ -18,6 +18,9 @@ namespace MiniDatabaseGui
 
         [DllImport("Project.dll")]
         private static extern void readFile2([In, Out]char[] arr);
+
+        [DllImport("Project.dll")]
+        private static extern void enroll2([In, Out]char[] id, [In, Out]char[] name,int size, int size2, [In, Out]char[] buf);
         public Form1()
         {
             InitializeComponent();
@@ -30,6 +33,20 @@ namespace MiniDatabaseGui
             readFile2(c);
             label1.Text = new string(c);
             MessageBox.Show(new string(c));
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {   char[] txt1 = textBox1.Text.ToCharArray();
+            char[] txt = textBox2.Text.ToCharArray();
+            char[] txt2 = new char[100];
+            enroll2(txt1, txt, txt1.Length, txt.Length, txt2);
+            label1.Text = new string(txt2);
+
         }
     }
 }
--- Project32_VS2015/AddTwo.asm
+++ Project32_VS2015/AddTwo.asm
@@ -1,56 +1,85 @@
 INCLUDE Irvine32.inc
 .data
-str1 byte  "ahmedFile.txt",0
-str2 byte  "data gwa file",0
+str1 byte  "ahmedFile.txt", 0
+str2 byte  "data gwa file", 0
 fileHandle handle ?
-BUFFER_SIZE = 100
+BUFFER_SIZE = 1000
 buffer byte 101 dup(0)
 
 
 .code
-makeFile proc
 
-mov edx,offset str1
+
+makeFile proc
+mov edx, offset str1
 INVOKE CreateFile,
 edx, GENERIC_WRITE, DO_NOT_SHARE, NULL,
 CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, 0
-mov fileHandle,eax
-mov eax,fileHandle
-mov edx,OFFSET str2
-mov ecx,sizeof str2
+mov fileHandle, eax
+mov eax, fileHandle
+mov edx, OFFSET str2
+mov ecx, sizeof str2
 call WriteToFile
-mov eax,fileHandle
+mov eax, fileHandle
 INVOKE CloseHandle, eax
 ret
 makefile endp
 
 AppendTxtFile proc
-mov edx,offset str1
+mov edx, offset str1
 INVOKE CreateFile,
 edx, GENERIC_READ, DO_NOT_SHARE, NULL,
 OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, 0
-mov fileHandle,eax
+mov fileHandle, eax
 ret
 AppendTxtFile endp
 
-readFile2 proc str12:PTR BYTE
-mov edx,offset str1
+readFile2 proc str12 : PTR BYTE
+mov edx, offset str1
 INVOKE CreateFile,
 edx, GENERIC_READ, DO_NOT_SHARE, NULL,
 OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, 0
-mov fileHandle,eax
-mov edx,str12
-mov ecx,100
+mov fileHandle, eax
+mov edx, str12
+mov ecx, 100
 call ReadFromFile
-mov buffer[eax],0
-mov eax,offset buffer
 
-mov eax,fileHandle
-
+mov buffer[eax], 0
+mov ebx, eax
+mov eax, offset buffer
+mov eax, fileHandle
 INVOKE CloseHandle, eax
-mov eax,offset buffer
+mov eax, ebx
 ret
 readFile2 endP
+
+enroll2 proc ID : ptr Byte, Namee : ptr Byte, IDSize : Dword, NameSize : Dword, buf: ptr Byte
+cld
+mov esi,  ID
+mov edi, offset buffer
+mov ecx, IDSize
+rep movsb
+
+mov al, ','
+mov [edi], al
+inc edi
+
+mov esi, Namee
+mov ecx, NameSize
+
+rep movsb
+mov eax,IDSize
+add eax, NameSize
+add eax,1
+mov esi ,offset buffer
+mov edi , buf
+mov ecx,eax
+rep movsb
+
+
+
+ret
+enroll2 endP
 
 
 
